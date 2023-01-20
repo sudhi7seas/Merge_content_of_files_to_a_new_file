2d46c615d95d3c94e86bfbf2594e0730af97fda4
sudhi777 <87425107+sudhi7seas@users.noreply.github.com>
2023-01-20 10:07:21 +0000

    Merging contents of file to a new file excluding header. Instead of multiple header, only one header at the top has been retained.

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-#step 1
 from pathlib import Path
-
-#step 1
 files_dir = Path('files')
 
-
-#step 2
 merged = ''
 
-#step 1
-for filepath in files_dir.iterdir():
+for index, filepath in enumerate (files_dir.iterdir()):
   with open(filepath, 'r') as file:
-    content = file.read()
+    #to get the list instead of a single string - use 'readlines()''
+    content = file.readlines()
 
-  #step 2
-  merged = merged + content + '\n' + '\n'
+    #Except the header, get the remaining contents of the files - use content[1:]
+    new_content = content[1:]
 
-#step 3
+    #in order to have the header only in the 1st file or only once in a merged file
+    if index == 0:
+      #to convert the list into the string
+      content = ''.join(content)
+      merged = merged + content + '\n'
+
+    else:
+      #to convert the list into the string
+      new_content = ''.join(new_content)
+      merged = merged + new_content + '\n'
+
 with open('merged.csv', 'w') as file:
   file.write(merged)
-  
